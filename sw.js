eced4256830b2466dc16e0b42a37c84dfbd5e4bc
mahelaissa <mahelaissa1@gmail.com>
2026-03-28 17:57:09 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,52 @@
-const cacheName = 'smart-kilimo-v9'; // Badili kila unapo-update
+const cacheName = 'smart-kilimo-v9';
 const assets = [
-  './',
+  '/',
   'index.html',
   'tf.min.js',
   'teachablemachine-image.min.js',
   'html2pdf.bundle.min.js',
   'manifest.json',
-  
- 'model/model.json',
-'model/metadata.json',
-'model/weights.bin'
-
+  'model/model.json',
+  'model/metadata.json',
+  'model/weights.bin',
+  // Unaweza kuongeza picha ya icon hapa pia
 ];
 
+// Install Service Worker
 self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(cacheName).then(cache => {
-      return Promise.all(
-        assets.map(asset => {
-          return cache.add(asset).catch(err => console.log("Faili halipo: " + asset));
-        })
-      );
+      console.log('Caching shell assets...');
+      // Kutumia addAll ni rahisi zaidi, lakini mbinu yako ya .map ni nzuri kwa debugging
+      return cache.addAll(assets);
     })
   );
   self.skipWaiting();
 });
 
+// Activate Event
 self.addEventListener('activate', evt => {
   evt.waitUntil(
     caches.keys().then(keys => {
-      return Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)));
+      return Promise.all(
+        keys.filter(key => key !== cacheName)
+            .map(key => caches.delete(key))
+      );
     })
   );
 });
 
+// Fetch Event
 self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
+      // Kama kipo kwenye cache, kirudishe. Kama hakipo, nenda mtandaoni.
+      return cacheRes || fetch(evt.request).catch(() => {
+        // Hapa unaweza kurudisha ukurasa wa "Opps! Hauna Mtandao" kwa HTML requests
+        if (evt.request.url.indexOf('.html') > -1) {
+          return caches.match('index.html'); 
+        }
+      });
     })
   );
 });

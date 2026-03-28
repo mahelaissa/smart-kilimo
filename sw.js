75a620af0a797f2d1c1f132569a676821a3d18e3
mahelaissa <mahelaissa1@gmail.com>
2026-03-28 19:44:12 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,33 @@
-const cacheName = 'smart-kilimo-v9';
+const cacheName = 'smart-kilimo-v10'; // Nimeongeza version ili browser isome upya
 const assets = [
-  '/',
-  'index.html',
-  'tf.min.js',
-  'teachablemachine-image.min.js',
-  'html2pdf.bundle.min.js',
-  'manifest.json',
-  'model/model.json',
-  'model/metadata.json',
-  'model/weights.bin',
-  // Unaweza kuongeza picha ya icon hapa pia
+  './',
+  './index.html',
+  './tf.min.js',
+  './teachablemachine-image.min.js',
+  './html2pdf.bundle.min.js',
+  './manifest.json',
+  './model/model.json',
+  './model/metadata.json',
+  './model/weights.bin'
 ];
 
 // Install Service Worker
 self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(cacheName).then(cache => {
-      console.log('Caching shell assets...');
-      // Kutumia addAll ni rahisi zaidi, lakini mbinu yako ya .map ni nzuri kwa debugging
-      return cache.addAll(assets);
+      console.log('Inahifadhi faili (Caching assets)...');
+      // Kutumia Promise.all na map hapa inasaidia kama faili moja ikikosekana nyingine ziendelee
+      return Promise.all(
+        assets.map(asset => {
+          return cache.add(asset).catch(err => console.error("Imeshindwa kuhifadhi:", asset, err));
+        })
+      );
     })
   );
   self.skipWaiting();
 });
 
-// Activate Event
+// Activate Event - Futa cache za zamani
 self.addEventListener('activate', evt => {
   evt.waitUntil(
     caches.keys().then(keys => {
@@ -36,15 +39,15 @@ self.addEventListener('activate', evt => {
   );
 });
 
-// Fetch Event
+// Fetch Event - Inasoma kutoka kwenye Cache hata kama hakuna Internet
 self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
-      // Kama kipo kwenye cache, kirudishe. Kama hakipo, nenda mtandaoni.
+      // Kama faili lipo kwenye cache, rudi nalo. Kama halipo, nenda mtandaoni (fetch)
       return cacheRes || fetch(evt.request).catch(() => {
-        // Hapa unaweza kurudisha ukurasa wa "Opps! Hauna Mtandao" kwa HTML requests
+        // Kama mtandao umekatika na anatafuta ukurasa wa HTML, mrudishe index.html
         if (evt.request.url.indexOf('.html') > -1) {
-          return caches.match('index.html'); 
+          return caches.match('./index.html'); 
         }
       });
     })
